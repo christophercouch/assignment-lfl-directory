637aa92e6380b9b3788e5db21e6687b20db718b0
Chris Couch <chrissofa22@gmail.com>
2022-02-17 09:24:27 -0700

    finalcommit

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
+// verify click action
+$("#verify-button").on('click', function() {
+    let employeeName = stringUppercase('#verify-name');
+    let answer = false;
+    for(i = 0; i < employeeList.length; i++) {
+        if(employeeList[i].name === employeeName) {
+            answer = true;
+        }
+    }
+    if(answer === true) {
+        alert(`${employeeName} is an employee.`);
+    } else {
+        alert(`${employeeName} is not an employee.`);
+    }
+})
+// delete click actions
+$('#delete-button').on('click', function() {
+    let answer = false,
+        index = -1,
+        employeeName = stringUppercase('#delete-name');
+    for(i = 0; i < employeeList.length; i++) {
+        // checks if employee exists
+        if(employeeList[i].name === employeeName) {
+            answer = true;
+            index = i;
+        }
+    }
+    if(answer === false) {
+        // notify user of non-existence
+        alert(`${employeeName} already does not exist.`);
+    } else {
+        // remove employee element
+        employeeList.splice(index, 1);
+        employeeRecycle();
+    }
+})
 // create employees
 employeeRecycle();
-
 $('#view').on('click', view);
 $('#add').on('click', add);
 $('#verify').on('click', verify);
@@ -19,71 +54,35 @@ $('#add-button').on('click', function() {
     // update employees
     employeeRecycle();
 })
-// verify click action
-$("#verify-button").on('click', function() {
-    let employeeName = stringUppercase('#verify-name');
-    let answer = false;
-    for (i = 0; i < employeeList.length; i++) {
-        if (employeeList[i].name === employeeName) {
-            answer = true;
-        }
-    }
-    if (answer === true) {
-        alert(`${employeeName} is an employee.`);
-    } else {
-        alert(`${employeeName} is not an employee.`);
-    }
-})
 // update click actions
 $('#update-button').on('click', function() {
-    let employeeName = stringUppercase('#update-name'),
-        employeeOffice = stringUppercase('#update-office'),
-        employeePhone = stringUppercase("#update-phone");
-    // set default false
-    let answer = false,
-        index = -1;
-    for (i = 0; i < employeeList.length; i++) {
-        // checks if employee exists
-        if (employeeList[i].name === employeeName) {
-            answer = true;
-            index = i;
+let employeeName = stringUppercase('#update-name'),
+    employeeOffice = stringUppercase('#update-office'),
+    employeePhone = stringUppercase("#update-phone");
+// set default false
+let answer = false,
+    index = -1;
+for(i = 0; i < employeeList.length; i++) {
+    // checks if employee exists
+    if(employeeList[i].name === employeeName) {
+        answer = true;
+        index = i;
+    }
+}
+if(answer === false) {
+    alert(`${employeeName} does not exist.`);
+} else {
+    if(employeePhone && employeeOffice) {
+        employeeList[index] = {
+            name: employeeName,
+            officeNumber: employeeOffice,
+            phoneNumber: employeePhone
         }
+    } else if(employeePhone) {
+        employeeList[index].phoneNumber = employeePhone;
+    } else if(employeeOffice) {
+        employeeList[index].officeNumber = employeeOffice;
     }
-    if (answer === false) {
-        alert(`${employeeName} does not exist.`);
-    } else {
-        if (employeePhone && employeeOffice) {
-            employeeList[index] = {
-                name: employeeName,
-                officeNumber: employeeOffice,
-                phoneNumber: employeePhone
-            }
-        } else if (employeePhone) {
-            employeeList[index].phoneNumber = employeePhone;
-        } else if (employeeOffice) {
-            employeeList[index].officeNumber = employeeOffice;
-        }
-    }
-    employeeRecycle();
-})
-// delete click actions
-$('#delete-button').on('click', function() {
-    let answer = false,
-        index = -1,
-        employeeName = stringUppercase('#delete-name');
-    for (i = 0; i < employeeList.length; i++) {
-        // checks if employee exists
-        if (employeeList[i].name === employeeName) {
-            answer = true;
-            index = i;
-        }
-    }
-    if (answer === false) {
-        // notify user of non-existence
-        alert(`${employeeName} already does not exist.`);
-    } else {
-        // remove employee element
-        employeeList.splice(index, 1);
-        employeeRecycle();
-    }
+}
+employeeRecycle();
 })
--- dom.js
+++ dom.js
@@ -1,90 +1,74 @@
 const $ = function(selector) {
-  const nodeList = document.querySelectorAll(selector);
-
-  const text = function(content) {
-      for (let i = 0; i < nodeList.length; i++) {
-          nodeList[i].innerText = content;
-      }
-  }
-
-  const html = function(content) {
-      for (let i = 0; i < nodeList.length; i++) {
-          nodeList[i].innerHTML = content;
-      }
-  }
-
-  const addClass = function(className) {
-      for (let i = 0; i < nodeList.length; i++) {
-          nodeList[i].classList.add(className);
-      }
-  }
-
-  const removeClass = function(className) {
-      for (let i = 0; i < nodeList.length; i++) {
-          nodeList[i].classList.remove(className);
-      }
-  }
-
-  const toggleClass = function(className) {
-      for (let i = 0; i < nodeList.length; i++) {
-          nodeList[i].classList.toggle(className);
-      }
-  }
-
-  const empty = function() {
-      for (let i = 0; i < nodeList.length; i++) {
-          nodeList[i].innerHTML = '';
-      }
-  }
-
-  const append = function(content) {
-      for (let i = 0; i < nodeList.length; i++) {
-          nodeList[i].innerHTML += content;
-      }
-  }
-
-  const prepend = function(content) {
-      for (let i = 0; i < nodeList.length; i++) {
-          nodeList[i].innerHTML = content + nodeList[i].innerHTML;
-      }
-  }
-
-  const val = function(content) {
-      if (content === undefined) {
-          return nodeList[0].value;
-      } else {
-          nodeList[0].value = content;
-      }
-  }
-
-  const on = function(action, cb) {
-      for (let i = 0; i < nodeList.length; i++) {
-          nodeList[i].addEventListener(action, cb);
-      }
-  }
-
-  return {
-      text: text,
-      html: html,
-      addClass: addClass,
-      removeClass: removeClass,
-      toggleClass: toggleClass,
-      empty: empty,
-      append: append,
-      prepend: prepend,
-      on: on,
-      val: val
-  };
+    const nodeList = document.querySelectorAll(selector);
+    const text = function(content) {
+        for(let i = 0; i < nodeList.length; i++) {
+            nodeList[i].innerText = content;
+        }
+    }
+    const html = function(content) {
+        for(let i = 0; i < nodeList.length; i++) {
+            nodeList[i].innerHTML = content;
+        }
+    }
+    const addClass = function(className) {
+        for(let i = 0; i < nodeList.length; i++) {
+            nodeList[i].classList.add(className);
+        }
+    }
+    const removeClass = function(className) {
+        for(let i = 0; i < nodeList.length; i++) {
+            nodeList[i].classList.remove(className);
+        }
+    }
+    const toggleClass = function(className) {
+        for(let i = 0; i < nodeList.length; i++) {
+            nodeList[i].classList.toggle(className);
+        }
+    }
+    const empty = function() {
+        for(let i = 0; i < nodeList.length; i++) {
+            nodeList[i].innerHTML = '';
+        }
+    }
+    const append = function(content) {
+        for(let i = 0; i < nodeList.length; i++) {
+            nodeList[i].innerHTML += content;
+        }
+    }
+    const prepend = function(content) {
+        for(let i = 0; i < nodeList.length; i++) {
+            nodeList[i].innerHTML = content + nodeList[i].innerHTML;
+        }
+    }
+    const val = function(content) {
+        if(content === undefined) {
+            return nodeList[0].value;
+        } else {
+            nodeList[0].value = content;
+        }
+    }
+    const on = function(action, cb) {
+        for(let i = 0; i < nodeList.length; i++) {
+            nodeList[i].addEventListener(action, cb);
+        }
+    }
+    return {
+        text: text,
+        html: html,
+        addClass: addClass,
+        removeClass: removeClass,
+        toggleClass: toggleClass,
+        empty: empty,
+        append: append,
+        prepend: prepend,
+        on: on,
+        val: val
+    };
 }
-
-function makeBox(object) {
-  let employee =
-      `<div class='section'>
-<p>Employee Name: ${object.name}</p>
-<p>Office Number: ${object.officeNumber}</p>
-<p>Phone Number: ${object.phoneNumber}</p>
-</div>`
-  return employee;
+// string grabber
+function stringUppercase(inputId) {
+const info = $(inputId).val();
+return autoUppercase(info);
 }
 // assigns IDs
 let employeeAdds = document.getElementById('ADD');
@@ -93,65 +77,68 @@ let employeeUpdates = document.getElementById('UPDATE');
 let employeeDeletes = document.getElementById('DELETE');
 let employeeNavigates = $("#left-menu");
 
-function employeeRecycle() {
-  $("#content").empty();
-  for (i = 0; i < employeeList.length; i++) {
-      $('#content').append(makeBox(employeeList[i]));
-  }
+function makeBox(object) {
+let employee = `<div class='section'>
+<p>Employee Name: ${object.name}</p>
+<p>Office Number: ${object.officeNumber}</p>
+<p>Phone Number: ${object.phoneNumber}</p>
+</div>`
+return employee;
 }
-// view function
-function view() {
-  employeeAdds.style.display = 'none';
-  employeeVerifies.style.display = 'none';
-  employeeUpdates.style.display = 'none';
-  employeeDeletes.style.display = 'none';
-  employeeRecycle();
-  employeeNavigates.removeClass("viewport");
 
+function employeeRecycle() {
+$("#content").empty();
+for(i = 0; i < employeeList.length; i++) {
+    $('#content').append(makeBox(employeeList[i]));
 }
-// add function
-function add() {
-  employeeAdds.style.display = 'block';
-  employeeVerifies.style.display = 'none';
-  employeeUpdates.style.display = 'none';
-  employeeDeletes.style.display = 'none';
-  employeeRecycle();
-  employeeNavigates.removeClass("viewport");
-}
-// verify function
-function verify() {
-  employeeAdds.style.display = 'none';
-  employeeVerifies.style.display = 'block';
-  employeeUpdates.style.display = 'none';
-  employeeDeletes.style.display = 'none';
-  $("#content").empty();
-  // applies Viewport height
-  employeeNavigates.addClass("viewport");
-}
-// vieupdatew function
-function update() {
-  employeeAdds.style.display = 'none';
-  employeeVerifies.style.display = 'none';
-  employeeUpdates.style.display = 'block';
-  employeeDeletes.style.display = 'none';
-  employeeRecycle();
-  employeeNavigates.removeClass("viewport");
-}
-// delete function
-function terminate() {
-  employeeAdds.style.display = 'none';
-  employeeVerifies.style.display = 'none';
-  employeeUpdates.style.display = 'none';
-  employeeDeletes.style.display = 'block';
-  employeeRecycle();
-  employeeNavigates.removeClass("viewport");
 }
 // auto uppercase strings
 function autoUppercase(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+return string.charAt(0).toUpperCase() + string.slice(1);
 }
-// string grabber
-function stringUppercase(inputId) {
-  const info = $(inputId).val();
-  return autoUppercase(info);
+// view function
+function view() {
+employeeAdds.style.display = 'none';
+employeeVerifies.style.display = 'none';
+employeeUpdates.style.display = 'none';
+employeeDeletes.style.display = 'none';
+employeeRecycle();
+employeeNavigates.removeClass("viewport");
+}
+// add function
+function add() {
+employeeAdds.style.display = 'block';
+employeeVerifies.style.display = 'none';
+employeeUpdates.style.display = 'none';
+employeeDeletes.style.display = 'none';
+employeeRecycle();
+employeeNavigates.removeClass("viewport");
+}
+// verify function
+function verify() {
+employeeAdds.style.display = 'none';
+employeeVerifies.style.display = 'block';
+employeeUpdates.style.display = 'none';
+employeeDeletes.style.display = 'none';
+$("#content").empty();
+// applies Viewport height
+employeeNavigates.addClass("viewport");
+}
+// vieupdatew function
+function update() {
+employeeAdds.style.display = 'none';
+employeeVerifies.style.display = 'none';
+employeeUpdates.style.display = 'block';
+employeeDeletes.style.display = 'none';
+employeeRecycle();
+employeeNavigates.removeClass("viewport");
+}
+// delete function
+function terminate() {
+employeeAdds.style.display = 'none';
+employeeVerifies.style.display = 'none';
+employeeUpdates.style.display = 'none';
+employeeDeletes.style.display = 'block';
+employeeRecycle();
+employeeNavigates.removeClass("viewport");
 }
